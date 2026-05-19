c3c8c6c97ad53f543648bcfa83c348753e01aa76
MikeJ94 <66752686+MikeJ94@users.noreply.github.com>
2026-05-19 12:06:44 -0600

    Fix import statement for healthRouter

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router, type IRouter } from "express";
-import healthRouter from "./health";
+//import healthRouter from "./health";
+import healthRouter from "./health.js";
 
 const router: IRouter = Router();
 
