b4e5c77daa50c6c208a8396f2842330152f1657b
MikeJ94 <66752686+MikeJ94@users.noreply.github.com>
2026-05-19 10:47:04 -0600

    Uncomment pino-http and router imports
    Uncomment import statements for pino-http and router.

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,8 +1,8 @@
 import express, { type Express } from "express";
 import cors from "cors";
-/*import pinoHttp from "pino-http";*/
+//import pinoHttp from "pino-http";
 import { pinoHttp } from 'pino-http';
-/*import router from "./routes";*/
+//import router from "./routes";
 // Por esto (añadiendo el archivo explícitamente):
 import router from './routes/index.js';
 import { logger } from "./lib/logger";
