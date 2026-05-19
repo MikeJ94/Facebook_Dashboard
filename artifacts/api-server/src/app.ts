aa8dc21e56475c56a60e56f94e7a92cbe2eab8c2
MikeJ94 <66752686+MikeJ94@users.noreply.github.com>
2026-05-19 10:45:05 -0600

    Fix import statement for router in app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import { pinoHttp } from 'pino-http';
 /*import router from "./routes";*/
 // Por esto (añadiendo el archivo explícitamente):
-import routes from './routes/index.js';
+import router from './routes/index.js';
 import { logger } from "./lib/logger";
 
 const app: Express = express();
