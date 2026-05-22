498449c94d21d6f09549ebc1039506459b6de780
MikeJ94 <66752686+MikeJ94@users.noreply.github.com>
2026-05-22 14:45:47 -0600

    Add comment for default export in index.js
    Added a comment in Spanish explaining the default export for Vercel's Serverless Function.

--- api/index.js
+++ api/index.js
@@ -1,3 +1,5 @@
+// api/index.js
 import app from '../artifacts/api-server/dist/index.mjs';
 
+// Exportamos por defecto para que la Serverless Function de Vercel tome el control
 export default app;
