01bf8e86e6417dee0615a08d9f3fe3a3b1988f1c
MikeJ94 <66752686+MikeJ94@users.noreply.github.com>
2026-05-22 15:08:39 -0600

    Simplify import of app from index.mjs

--- artifacts/api-server/api/index.ts
+++ artifacts/api-server/api/index.ts
@@ -1,10 +1,4 @@
-import path from 'path';
-import { createRequire } from 'module';
+// artifacts/api-server/api/index.js
+import app from '../dist/index.mjs';
 
-// Forzamos la lectura usando la ruta absoluta del proceso en el servidor de Vercel
-const require = createRequire(import.meta.url);
-const appPath = path.resolve(process.cwd(), 'artifacts/api-server/dist/index.mjs');
-
-const app = await import(appPath);
-
-export default app.default || app;
+export default app;
